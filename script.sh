5c058353374b830b30712476b4aad0ea51c61576
gatien <gatien.deleau@edu.saintluc-cambrai.com>
2024-11-06 10:54:50 +0100

    Modifie script.sh pour corriger un problème dans fix5

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script to add a user to Linux system
 if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
+	read -p "Enter username : " gat
 	read -s -p "Enter password : " password
 	egrep "^$username" /etc/passwd >/dev/null
 	if [ $? -eq 0 ]; then
